f8874d1235adf184951530fb0af14e919a6bf1d5
Mntndew <trickepiclyawesome@gmail.com>
2014-06-14 05:11:17 +0200

    Basic functionality.

---
+++ Clipboard Stasher.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Clipboard_Stasher
+{
+    partial class ClipboardStasher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_history = new System.Windows.Forms.ListBox();
+            this.m_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // m_history
+            // 
+            this.m_history.FormattingEnabled = true;
+            this.m_history.Location = new System.Drawing.Point(12, 12);
+            this.m_history.Name = "m_history";
+            this.m_history.Size = new System.Drawing.Size(260, 212);
+            this.m_history.TabIndex = 0;
+            this.m_history.SelectedIndexChanged += new System.EventHandler(this.m_history_SelectedIndexChanged);
+            // 
+            // m_clear
+            // 
+            this.m_clear.Location = new System.Drawing.Point(12, 231);
+            this.m_clear.Name = "m_clear";
+            this.m_clear.Size = new System.Drawing.Size(260, 23);
+            this.m_clear.TabIndex = 1;
+            this.m_clear.Text = "Clear";
+            this.m_clear.UseVisualStyleBackColor = true;
+            this.m_clear.Click += new System.EventHandler(this.m_clear_Click);
+            // 
+            // ClipboardStasher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 266);
+            this.Controls.Add(this.m_clear);
+            this.Controls.Add(this.m_history);
+            this.Name = "ClipboardStasher";
+            this.Text = "Clipboard Stasher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox m_history;
+        private System.Windows.Forms.Button m_clear;
+
+
+    }
+}
+
---
+++ Clipboard Stasher.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Clipboard_Stasher
+{
+    public partial class ClipboardStasher : Form
+    {
+        private NotificationForm _form;
+
+        public ClipboardStasher()
+        {
+            InitializeComponent();
+            _form = new NotificationForm(m_history.Items);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private class NotificationForm : Form
+        {
+            public NotificationForm(ListBox.ObjectCollection p_history)
+            {
+                m_history = p_history;
+                NativeMethods.SetParent(Handle, NativeMethods.HWND_MESSAGE);
+                NativeMethods.AddClipboardFormatListener(Handle);
+            }
+
+            static ListBox.ObjectCollection m_history;
+
+            public static event EventHandler ClipboardUpdate;
+
+            private static void OnClipboardUpdate(EventArgs e)
+            {
+                var handler = ClipboardUpdate;
+                if (handler != null)
+                {
+                    handler(null, e);
+                }
+                m_history.Add((String)System.Windows.Forms.Clipboard.GetData(System.Windows.Forms.DataFormats.StringFormat));
+            }
+
+            protected override void WndProc(ref Message m)
+            {
+                if (m.Msg == NativeMethods.WM_CLIPBOARDUPDATE)
+                {
+                    OnClipboardUpdate(null);
+                }
+                base.WndProc(ref m);
+            }
+        }
+
+        private void m_history_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetData(System.Windows.Forms.DataFormats.StringFormat, m_history.SelectedItem.ToString());
+        }
+
+        private void m_clear_Click(object sender, EventArgs e)
+        {
+            m_history.Items.Clear();
+        }
+    }
+
+    internal static class NativeMethods
+    {
+        public const int WM_CLIPBOARDUPDATE = 0x031D;
+        public static IntPtr HWND_MESSAGE = new IntPtr(-3);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+    }
+}
